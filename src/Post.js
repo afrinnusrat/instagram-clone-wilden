1a6c79c3d5978a1122e7cf67fc72add609e08500
wildenali <wildenwildenaliali@gmail.com>
2020-08-30 14:32:09 +0700

    membuat css untuk img post

--- src/Post.js
+++ src/Post.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import './Post.css'
 
 function Post() {
   return (
-    <div>
+    <div className="post">
       {/* Header -> Avatar + Username */}
       <h3>Username</h3>
 
       {/* Image */}
+      <img className="post__image" src="https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg" />
 
       {/* Username + Caption */}
+      <h4>Username: caption</h4>
     </div>
   )
 }
