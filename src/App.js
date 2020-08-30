57c5e1cc7a601949aef842cb3a23a546b7244bd3
wildenali <wildenwildenaliali@gmail.com>
2020-08-30 23:10:58 +0700

    ganti dari <Post blablabla> jadi pake useState function, supaya lebih praktis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Post from './Post';
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      username: "aliwilden",
+      caption: "Hey wow it works",
+      imageUrl: "https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg"
+    },
+    {
+      username: "kairav",
+      caption: "mantul sekali nih",
+      imageUrl: "https://oecdenvironmentfocusblog.files.wordpress.com/2020/06/wed-blog-shutterstock_1703194387_low_nwm.jpg?w=640"
+    }
+  ]);
+
   return (
     <div className="app">
       <div className="app__header">
@@ -13,9 +27,13 @@ function App() {
         />
       </div>
       <h1>Instagram Clone Wilden 🔥 🚀</h1>
-      <Post username="aliwilden" caption="Hey wow it works" imageUrl="https://scx2.b-cdn.net/gfx/news/hires/2019/2-nature.jpg" />
-      <Post username="wulan" caption="mantul sekali nih" imageUrl="https://oecdenvironmentfocusblog.files.wordpress.com/2020/06/wed-blog-shutterstock_1703194387_low_nwm.jpg?w=640" />
-      <Post username="murtado" caption="Bisa bekerja dengan baik" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBvjD2CimTjRuPMRx1PbMLEBB3wWSBrAnzuw&usqp=CAU" />
+      
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        ))
+      }
+      
     </div>
   );
 }
