4a22139afb2447f781bb28add402d360c63141be
wildenali <wildenwildenaliali@gmail.com>
2020-08-31 15:37:23 +0700

    nge clear input tag ketika telah selesai upload gambar

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -48,10 +48,10 @@ function ImageUpload({username}) {
               username: username
             });
 
-            setProgress(0);
             setCaption('');
             setImage(null);
-          })
+            setProgress(0);
+          });
       }
     )
   }
@@ -65,7 +65,7 @@ function ImageUpload({username}) {
       {/* Post Button */}
 
       <progress value={progress} max="100" />
-      <input type="text" placeholder='Enter a caption...' onChange={event => setCaption(event.target.value)} />
+      <input type="text" placeholder='Enter a caption...' onChange={event => setCaption(event.target.value)} value={caption} />
       <input type="file" onChange={handleChange} />
       <Button onClick={handleUpload}>
         Upload
