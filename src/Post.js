3d34b70a48f45838f897abf9c98a7c5e77fa5645
wildenali <wildenwildenaliali@gmail.com>
2020-08-31 17:10:57 +0700

    add comment input

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Post.css'
 import Avatar from '@material-ui/core/Avatar';
+import { db } from './firebase';
 
-function Post({ username, caption, imageUrl }) {
+function Post({postId,  username, caption, imageUrl }) {
+
+  const [comments, setComments] = useState([])
+  const [comment, setComment] = useState('')
+
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+        .collection("posts")
+        .doc(postId)
+        .collection("comments")
+        .onSnapshot((snapshot) => {
+          setComments(snapshot.docs.map((doc) => doc.data()))
+        });
+    }
+
+    return () => {
+      unsubscribe();
+    }
+  }, [postId])
+
   return (
     <div className="post">
       {/* Header -> Avatar + Username */}
@@ -20,6 +42,16 @@ function Post({ username, caption, imageUrl }) {
 
       {/* Username + Caption */}
       <h4 className="post__text"><strong>{username} </strong>{caption}</h4>
+
+      <form>
+        <input
+          className="post__input"
+          type="text"
+          placeholder="Add a commet..."
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+        />
+      </form>
     </div>
   )
 }
